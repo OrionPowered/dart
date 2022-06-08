fa58034be06174d64b790b5a683c779fe2eb80be
Alex Sobiek <alex@alexsobiek.com>
2022-06-08 03:25:49 -0500

    Depend on lombok

--- b1.1_02/build.gradle.kts
+++ b1.1_02/build.gradle.kts
@@ -8,3 +8,8 @@ plugins {
 dart {
     serverVersion.set(Version.BETA1_1__02)
 }
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+}
