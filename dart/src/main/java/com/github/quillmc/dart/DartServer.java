b61c0eb096c53bb70be0b21fa0a67654c704df2f
Alex Sobiek <alex@alexsobiek.com>
2022-06-09 04:24:36 -0500

    Created Abstract DartServer

---
+++ dart/src/main/java/com/github/quillmc/dart/DartServer.java
@@ -0,0 +1,52 @@
+package com.github.quillmc.dart;
+
+import com.alexsobiek.async.AsyncUtility;
+import com.alexsobiek.async.event.EventBus;
+import com.github.quillmc.dart.api.Server;
+import com.github.quillmc.dart.api.entity.Player;
+import com.github.quillmc.dart.api.event.PlayerChatEvent;
+import com.github.quillmc.dart.api.event.PlayerJoinEvent;
+import com.github.quillmc.dart.api.event.PlayerLeaveEvent;
+import com.github.quillmc.dart.chat.ChatFormatter;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+
+import java.util.Optional;
+
+@Setter
+@Getter
+public abstract class DartServer<P extends Player> implements Server<P> {
+    private final AsyncUtility async = AsyncUtility.builder().build();
+    private ChatFormatter<P> chatFormatter = (ChatFormatter<P>) ChatFormatter.DEFAULT;
+
+    public void preStart() {
+    }
+
+    @Override
+    public EventBus getEventBus() {
+        return async.eventBus();
+    }
+
+    @SneakyThrows
+    protected boolean handleJoin(P player) {
+        PlayerJoinEvent<P> e = getEventBus().post(new PlayerJoinEvent<>(player)).get();
+        return !e.isCancelled();
+    }
+
+    protected void handleLeave(P player) {
+        getEventBus().post(new PlayerLeaveEvent<>(player));
+    }
+
+    @SneakyThrows
+    protected Optional<String> handleChat(P player, String chat) {
+        PlayerChatEvent<P> e = getEventBus().post(new PlayerChatEvent<>(player, chat)).get();
+        broadcast("test");
+        if (e.isCancelled()) return Optional.empty();
+        else return Optional.of(chatFormatter.format(player, chat));
+    }
+
+    public abstract void broadcast(String msg);
+
+    public abstract void broadcastPlain(String msg);
+}
