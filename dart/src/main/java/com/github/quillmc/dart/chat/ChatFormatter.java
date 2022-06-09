c288fe48ac013746861f8d8ea21ce5e86c4e6995
Alex Sobiek <alex@alexsobiek.com>
2022-06-09 04:22:27 -0500

    Created ChatFormatter

---
+++ dart/src/main/java/com/github/quillmc/dart/chat/ChatFormatter.java
@@ -0,0 +1,9 @@
+package com.github.quillmc.dart.chat;
+
+import com.github.quillmc.dart.api.entity.Player;
+
+public interface ChatFormatter<P extends Player> {
+    public static ChatFormatter<Player> DEFAULT = (player, message) -> String.format("<%s> %s", player.getUsername(), message);
+
+    String format(P player, String message);
+}
