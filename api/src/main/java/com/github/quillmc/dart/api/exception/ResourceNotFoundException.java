a161684a81e6bf2589a762ebd304052018a725c9
Alex Sobiek <alex@alexsobiek.com>
2022-06-09 05:06:53 -0500

    Created ResourceNotFoundException

---
+++ api/src/main/java/com/github/quillmc/dart/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.quillmc.dart.api.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
