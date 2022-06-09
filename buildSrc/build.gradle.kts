530ee1b1e4784448b0adc60f91ef567041df0a90
Alex Sobiek <alex@alexsobiek.com>
2022-06-08 19:02:27 -0500

    Bump TinyMCP version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("gradle.plugin.com.github.monosoul:yadegrap:1.0.0")
     implementation("org.eclipse.jgit:org.eclipse.jgit:6.1.0.202203080745-r")
-    implementation("com.github.QuillMC:TinyMCP:50d135b481")
+    implementation("com.github.QuillMC:TinyMCP:6b7af2d257")
     implementation("net.fabricmc:cfr:0.1.1")
 }
 
