12961657cf82fe6815595a226357b32227a0a261
Alex Sobiek <alex@alexsobiek.com>
2022-06-09 19:18:08 -0500

    Added server version support

--- api/src/main/java/com/github/quillmc/dart/api/Server.java
+++ api/src/main/java/com/github/quillmc/dart/api/Server.java
@@ -1,13 +1,15 @@
 package com.github.quillmc.dart.api;
 
 import com.alexsobiek.async.event.EventBus;
-import com.github.quillmc.dart.api.entity.Player;
 import com.github.quillmc.dart.api.chat.ChatFormatter;
+import com.github.quillmc.dart.api.entity.Player;
 
 import java.util.Optional;
 
 public interface Server<P extends Player> {
+
     EventBus getEventBus();
+
     Optional<? extends P> getPlayer(String username);
 
     ChatFormatter<P> getChatFormatter();
@@ -17,4 +19,20 @@ public interface Server<P extends Player> {
     void broadcast(String msg);
 
     void broadcastPlain(String msg);
+
+    // Version Interfaces
+    // Quick note: essentially objects such as Player will vary across different versions. We can number the versions
+    // and plugins can then choose which version to use.
+
+    /**
+     * Base Server version supporting all Dart servers
+     */
+    interface All extends Server<Player> {
+    }
+
+    /**
+     * V1 Dart Server. Same thing as {@link Server.All}
+     */
+    interface V1 extends All {
+    }
 }
